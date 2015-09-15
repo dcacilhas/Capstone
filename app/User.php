413af21b997e4dcc167d2d6ab528dc953b45de7a
David Cacilhas <dcacilhas@gmail.com>
2015-09-14 22:16:02 -0400

    Update User model

--- app/User.php
+++ app/User.php
@@ -3,12 +3,12 @@
 namespace App;
 
 use Illuminate\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
-use Illuminate\Foundation\Auth\Access\Authorizable;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
 {
@@ -26,7 +26,19 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      *
      * @var array
      */
-    protected $fillable = ['username', 'email', 'password'];
+    protected $fillable = [
+        'username',
+        'email',
+        'password',
+        'about',
+        'birthday',
+        'location',
+        'gender',
+        'avatar_path',
+        'notification_email',
+        'profile_visibility',
+        'list_visibility'
+    ];
 
     /**
      * The attributes excluded from the model's JSON form.
