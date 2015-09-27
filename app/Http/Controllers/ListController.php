fd2247bce5b435beb9027b355328d747acaf6516
David Cacilhas <dcacilhas@gmail.com>
2015-09-26 23:33:48 -0400

    Add List view with temporary table data

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests;
 use App\User;
+use Input;
 
 class ListController extends Controller
 {
@@ -14,8 +15,83 @@ class ListController extends Controller
 
     public function index($username)
     {
-        $user = User::where('username', $username)->first();
+        // TODO: Remove. Test data for table structure. Replace with actual Model info from database.
+        $series = array(
+            array(
+                'list_status' => 0,
+                'SeriesName' => 'Watching 1',
+                'rating' => 7,
+                'last_episode_watched' => 'S01E02',
+                'progress' => '1/100'
+            ),
+            array(
+                'list_status' => 0,
+                'SeriesName' => 'Watching 2',
+                'rating' => 8,
+                'last_episode_watched' => 'S01E02',
+                'progress' => '2/100'
+            ),
+            array(
+                'list_status' => 0,
+                'SeriesName' => 'Watching 3',
+                'rating' => 9,
+                'last_episode_watched' => 'S01E03',
+                'progress' => '3/100'
+            ),
+            array(
+                'list_status' => 1,
+                'SeriesName' => 'Plan To Watch 1',
+                'rating' => 5,
+                'last_episode_watched' => 'S01E01',
+                'progress' => '3/100'
+            ),
+            array(
+                'list_status' => 1,
+                'SeriesName' => 'Plan To Watch 2',
+                'rating' => 7.6,
+                'last_episode_watched' => 'S02E03',
+                'progress' => '7/100'
+            ),
+            array(
+                'list_status' => 2,
+                'SeriesName' => 'Completed 1',
+                'rating' => 6,
+                'last_episode_watched' => 'S01E03',
+                'progress' => '3/100'
+            ),
+            array(
+                'list_status' => 2,
+                'SeriesName' => 'Completed 2',
+                'rating' => 8,
+                'last_episode_watched' => 'S01E03',
+                'progress' => '3/100'
+            ),
+            array(
+                'list_status' => 2,
+                'SeriesName' => 'Completed 3',
+                'rating' => 9,
+                'last_episode_watched' => 'S01E03',
+                'progress' => '3/100'
+            ),
+            array(
+                'list_status' => 3,
+                'SeriesName' => 'On Hold 1',
+                'rating' => 6,
+                'last_episode_watched' => 'S01E03',
+                'progress' => '3/100'
+            ),
+            array(
+                'list_status' => 3,
+                'SeriesName' => 'On Hold 2',
+                'rating' => 7,
+                'last_episode_watched' => 'S01E03',
+                'progress' => '3/100'
+            ),
+        );
 
-        return view('profile/list', ['user' => $user]);
+        $user = User::where('username', $username)->first();
+        $status = Input::get('status');
+
+        return view('profile/list', ['user' => $user, 'series' => $series, 'status' => $status]);
     }
 }
--- resources/views/profile/list.blade.php
+++ resources/views/profile/list.blade.php
@@ -5,5 +5,142 @@
 @section('content')
     <div class="container">
         @include('includes.profile_submenu')
+
+            <ul class="nav nav-pills nav-justified">
+                <li role="presentation"
+                    @if($status === null) class="active" @endif>{!! link_to_route('profile/list', 'All', ['username' => $user['username']]) !!}</li>
+                <li role="presentation"
+                    @if($status === '0') class="active" @endif>{!! link_to_route('profile/list', 'Watching', ['username' => $user['username'], 'status' => '0']) !!}</li>
+                <li role="presentation"
+                    @if($status === '1') class="active" @endif>{!! link_to_route('profile/list', 'Plan To Watch', ['username' => $user['username'], 'status' => '1']) !!}</li>
+                <li role="presentation"
+                    @if($status === '2') class="active" @endif>{!! link_to_route('profile/list', 'Completed', ['username' => $user['username'], 'status' => '2']) !!}</li>
+                <li role="presentation"
+                    @if($status === '3') class="active" @endif>{!! link_to_route('profile/list', 'On Hold', ['username' => $user['username'], 'status' => '3']) !!}</li>
+            </ul>
+
+            @if($status === '0' || $status === null)
+                <table class="table table-striped">
+                    <caption>Watching</caption>
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Series Title</th>
+                        <th>Rating</th>
+                        <th>Last Episode Watched</th>
+                        <th>Progress</th>
+                        <th>Edit</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php $i = 1; ?>
+                    @foreach($series as $s)
+                        @if($s['list_status'] === 0)
+                            <tr>
+                                <th scope="row">{{ $i++ }}</th>
+                                <td>{{ $s['SeriesName'] }}</td>
+                                <td>{{ $s['rating'] }}</td>
+                                <td>{{ $s['last_episode_watched'] }}</td>
+                                <td>{{ $s['progress'] }}</td>
+                                <td></td>
+                            </tr>
+                        @endif
+                    @endforeach
+                    </tbody>
+                </table>
+            @endif
+
+            @if($status === '1' || $status === null)
+                <table class="table table-striped">
+                    <caption>Plan To Watch</caption>
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Series Title</th>
+                        <th>Rating</th>
+                        <th>Last Episode Watched</th>
+                        <th>Progress</th>
+                        <th>Edit</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php $i = 1; ?>
+                    @foreach($series as $index => $s)
+                        @if($s['list_status'] === 1)
+                            <tr>
+                                <th scope="row">{{ $i++ }}</th>
+                                <td>{{ $s['SeriesName'] }}</td>
+                                <td>{{ $s['rating'] }}</td>
+                                <td>{{ $s['last_episode_watched'] }}</td>
+                                <td>{{ $s['progress'] }}</td>
+                                <td></td>
+                            </tr>
+                        @endif
+                    @endforeach
+                    </tbody>
+                </table>
+            @endif
+
+            @if($status === '2' || $status === null)
+                <table class="table table-striped">
+                    <caption>Completed</caption>
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Series Title</th>
+                        <th>Rating</th>
+                        <th>Last Episode Watched</th>
+                        <th>Progress</th>
+                        <th>Edit</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php $i = 1; ?>
+                    @foreach($series as $index => $s)
+                        @if($s['list_status'] === 2)
+                            <tr>
+                                <th scope="row">{{ $i++ }}</th>
+                                <td>{{ $s['SeriesName'] }}</td>
+                                <td>{{ $s['rating'] }}</td>
+                                <td>{{ $s['last_episode_watched'] }}</td>
+                                <td>{{ $s['progress'] }}</td>
+                                <td></td>
+                            </tr>
+                        @endif
+                    @endforeach
+                    </tbody>
+                </table>
+            @endif
+
+            @if($status === '3' || $status === null)
+                <table class="table table-striped">
+                    <caption>On Hold</caption>
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Series Title</th>
+                        <th>Rating</th>
+                        <th>Last Episode Watched</th>
+                        <th>Progress</th>
+                        <th>Edit</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php $i = 1; ?>
+                    @foreach($series as $index => $s)
+                        @if($s['list_status'] === 3)
+                            <tr>
+                                <th scope="row">{{ $i++ }}</th>
+                                <td>{{ $s['SeriesName'] }}</td>
+                                <td>{{ $s['rating'] }}</td>
+                                <td>{{ $s['last_episode_watched'] }}</td>
+                                <td>{{ $s['progress'] }}</td>
+                                <td></td>
+                            </tr>
+                        @endif
+                    @endforeach
+                    </tbody>
+                </table>
+            @endif
     </div>
 @stop
