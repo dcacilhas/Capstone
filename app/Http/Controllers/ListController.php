5fa2761a9fc34925b4b9f6d38efebb1a90341049
David Cacilhas <dcacilhas@gmail.com>
2015-09-29 03:47:59 -0400

    Refactor ListController

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests;
-use App\Lists;
 use App\User;
 use DB;
 use Input;
@@ -20,23 +19,21 @@ class ListController extends Controller
         $user = User::where('username', $username)->first();
         $status = Input::get('status');
         $listStatuses = DB::table('list_statuses')->get();
+        $listStatusesArray = DB::table('list_statuses')->lists('list_status');
+        $listStatusesArray = array_map('strval', $listStatusesArray);   // Convert to string for comparison
+        $series = User::find(1)->getList()
+            ->join('tvseries', 'list.series_id', '=', 'tvseries.id')
+            ->where('user_id', $user->id);
 
-        // TODO: Clean this up. Should be passing pre-filtered (by status) series' to view.
-        if (!in_array($status, ['0', '1', '2', '3'], true)) {
-            $status = null;
-
-            $series = Lists::join('tvseries', 'list.series_id', '=', 'tvseries.id')
-                ->select('list.*', 'tvseries.SeriesName')
-                ->where('user_id', $user->id)
-                ->get();
+        // Filter which series info to retrieve based on list_status
+        if (in_array($status, $listStatusesArray, true)) {
+            $series = $series->where('list_status', $status)->get();
         } else {
-            $series = Lists::join('tvseries', 'list.series_id', '=', 'tvseries.id')
-                ->select('list.*', 'tvseries.SeriesName')
-                ->where('user_id', $user->id)
-                ->where('list_status', (int)$status)
-                ->get();
+            $status = null;
+            $series = $series->get();
         }
 
-        return view('profile/list', ['user' => $user, 'series' => $series, 'status' => $status, 'listStatuses' => $listStatuses]);
+        return view('profile/list',
+            ['user' => $user, 'series' => $series, 'status' => $status, 'listStatuses' => $listStatuses]);
     }
 }
