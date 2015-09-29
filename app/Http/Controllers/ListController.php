6f73ff605b2d92d5c5bb01d094777f8ffbe72c76
David Cacilhas <dcacilhas@gmail.com>
2015-09-29 04:12:35 -0400

    Refactor List controller to be more Eloquent

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -19,18 +19,18 @@ class ListController extends Controller
         $user = User::where('username', $username)->first();
         $status = Input::get('status');
         $listStatuses = DB::table('list_statuses')->get();
-        $listStatusesArray = DB::table('list_statuses')->lists('list_status');
-        $listStatusesArray = array_map('strval', $listStatusesArray);   // Convert to string for comparison
-        $series = User::find(1)->getList()
+        $series = User::find($user->id)->getList()
             ->join('tvseries', 'list.series_id', '=', 'tvseries.id')
-            ->where('user_id', $user->id);
+            ->where('user_id', $user->id)
+            ->get();
 
-        // Filter which series info to retrieve based on list_status
-        if (in_array($status, $listStatusesArray, true)) {
-            $series = $series->where('list_status', $status)->get();
-        } else {
-            $status = null;
-            $series = $series->get();
+        // Only get series that match by filtered status, otherwise return all
+        if ($status != null && $series->contains('list_status', (int)$status)) {
+            $series = User::find($user->id)->getList()
+                ->join('tvseries', 'list.series_id', '=', 'tvseries.id')
+                ->where('user_id', $user->id)
+                ->where('list_status', $status)
+                ->get();
         }
 
         return view('profile/list',
