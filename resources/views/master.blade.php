66ea7a88de249df5bf534f513a5555428bebe120
David Cacilhas <dcacilhas@gmail.com>
2015-10-03 11:28:23 -0400

    Add CSS/JS sections to master page

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -51,6 +51,7 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    @yield('css')
 </head>
 
 <body>
@@ -100,5 +101,6 @@
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+@yield('javascript')
 </body>
 </html>
