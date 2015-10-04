5528d5cbb70a93492ba09d595b1f591c8ff30d60
David Cacilhas <dcacilhas@gmail.com>
2015-10-04 15:20:45 -0400

    Display shows in table; Add filtering by genre; Add to Show modal

--- app/Http/Controllers/ShowsController.php
+++ app/Http/Controllers/ShowsController.php
@@ -3,25 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests;
+use App\Lists;
 use App\Show;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Input;
 
 class ShowsController extends Controller
 {
     public function index()
     {
+        $user = Auth::user();
+        $genres = DB::table('genres')->get();
         $filter = Input::get('filter');
+        $genre = Input::get('genre');
         $filters = range('A', 'Z');
         array_unshift($filters, '#');
+        $listStatuses = DB::table('list_statuses')->get();
 
         if (in_array(strtoupper($filter), $filters)) {
             $shows = ($filter === '#') ? Show::whereRaw("SeriesName REGEXP '^[0-9]+'") : Show::where('SeriesName',
                 'LIKE', $filter . '%');
             $shows = $shows->orderBy('SeriesName', 'asc')->paginate(50);
 
-            return view('shows', ['filters' => $filters, 'shows' => $shows, 'selectedFilter' => urlencode($filter)]);
+            return view('shows', ['user' => $user, 'genres' => $genres, 'filters' => $filters, 'shows' => $shows, 'selectedFilter' => urlencode($filter), 'listStatuses' => $listStatuses]);
         }
 
-        return view('shows', ['filters' => $filters]);
+        if (!empty($genre)) {
+            $shows = Show::where('Genre', 'LIKE', '%' . $genre . '%')
+                ->orderBy('SeriesName', 'asc')
+                ->whereNotNull('SeriesID')
+                ->whereNotNull('SeriesName')
+                ->where('SeriesName', 'NOT LIKE', '%*')
+                ->where('SeriesName', '<>', '')
+                ->paginate(50);
+
+            // Check if show exists in user's list so we can display the Add Show button or not
+            foreach ($shows as $show) {
+                $show->is_in_list = $is_in_list = Lists::where('user_id', $user->id)->where('series_id', $show->id)->exists();
+            }
+
+            return view('shows', ['user' => $user, 'genres' => $genres, 'selectedGenre' => $genre, 'filters' => $filters, 'shows' => $shows, 'listStatuses' => $listStatuses]);
+        }
+
+        return view('shows', ['user' => $user, 'genres' => $genres, 'filters' => $filters, 'listStatuses' => $listStatuses]);
     }
 }
--- resources/views/shows.blade.php
+++ resources/views/shows.blade.php
@@ -5,20 +5,131 @@
 @section('content')
     <div class="container">
         <h1>TV Shows</h1>
+
         <!-- TODO: Add filters (All Shows, Genres, Top Rated) -->
+        <ul class="nav nav-pills nav-justified">
+            {{--<li role="presentation" class="{{ (Request::route()->getName() == 'profile/list') ? 'active' : '' }}">{!! link_to_route('profile/list', 'List', ['username' => $user['username']]) !!}</li>--}}
+            <li role="presentation"><a href="#">All Shows</a></li>
+            <li role="presentation" class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                    Genres <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu">
+                    @foreach($genres as $genre)
+                        <li>{!! link_to_route('shows', $genre->genre, array('genre' => $genre->genre)) !!}</li>
+                    @endforeach
+                </ul>
+            </li>
+        </ul>
+
         @foreach ($filters as $filter)
             {!! link_to_route('shows', $filter, array('filter' => $filter)) !!}
         @endforeach
         @if (isset($shows))
-            <ul>
-                @foreach ($shows as $show)
-                    <li>{{ $show->SeriesName }}</li>
+            <table class="table table-striped table-bordered">
+                <caption>Shows</caption>
+                <thead>
+                <tr>
+                    <th class="text-center">#</th>
+                    <th class="col-md-6">Series Title</th>
+                    <th class="col-md-1 text-center">Status</th>
+                    <th class="col-md-1 text-center">FirstAired</th>
+                    <th class="col-md-1 text-center">Network</th>
+                    <th class="col-md-1 text-center">Runtime (mins)</th>
+                    <th class="col-md-1 text-center">Rating</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php $i = $shows->firstItem(); ?>
+                @foreach($shows as $show)
+                    <tr>
+                        <th scope="row" class="text-center">{{ $i++ }}</th>
+                        <td>
+                            {{ $show->SeriesName }}
+                            @if ((Auth::check() && Auth::user()->username === $user->username))
+                                @if (!$show->is_in_list)
+                                <div class="pull-right">
+                                    <a href="#" class="edit"
+                                       data-toggle="modal"
+                                       data-target="#addModal"
+                                       data-series-id="{{ $show->id }}"
+                                       data-series-title="{{ $show->SeriesName }}"
+                                       title="Add to List">
+                                        <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                                    </a>
+                                </div>
+                                @endif
+                            @endif
+                        </td>
+                        <td class="text-center">{{ $show->Status }}</td>
+                        <td class="text-center">{{ $show->FirstAired }}</td>
+                        <td class="text-center">{{ $show->Network }}</td>
+                        <td class="text-center">{{ $show->Runtime }}</td>
+                        <td class="text-center">{{ $show->Rating }}</td>
+                    </tr>
                 @endforeach
-            </ul>
+                </tbody>
+            </table>
 
-            {!! $shows->appends(['filter' => $selectedFilter])->render() !!}
+            @if (isset($selectedFilter))
+                {!! $shows->appends(['filter' => $selectedFilter])->render() !!}
+            @else
+                {!! $shows->appends(['genre' => $selectedGenre])->render() !!}
+            @endif
         @else
             <p>Click to filter shows starting with that letter.</p>
         @endif
+
+            <!-- AddModal -->
+            <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title" id="addModalLabel"></h4>
+                        </div>
+                        <div class="modal-body">
+                            {!! Form::open(['route' => ['profile/addToList', $user->username], 'class' => 'form-horizontal']) !!}
+
+                            {!! Form::hidden('series_id', null, ['id' => 'series_id']) !!}
+
+                            <div class="form-group">
+                                {!! Form::label('status', 'Status: ', ['class' => 'col-sm-2 control-label']) !!}
+
+                                <div class="col-sm-10">
+                                    @foreach($listStatuses as $listStatus)
+                                        <div class="radio">
+                                            <label>
+                                                {!! Form::radio('status', $listStatus->list_status, null, ['id' => 'status_' . $listStatus->list_status]) !!}
+                                                {{ $listStatus->description }}
+                                            </label>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            {!! Form::submit('Add Show To List', ['class' => 'btn btn-primary']) !!}
+                        </div>
+
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
     </div>
+@stop
+
+@section('javascript')
+    <script>
+        $('#addModal').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget),
+                    title = button.data('series-title'),
+                    id = button.data('series-id'),
+                    modal = $(this);
+            modal.find('#series_id').val(id);
+            modal.find('.modal-title').text(title);
+            modal.find('#status_0').prop('checked', true);
+        });
+    </script>
 @stop
