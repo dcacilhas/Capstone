077bc5e72c9f7174848c018998cecf66ddf134c1
David Cacilhas <dcacilhas@gmail.com>
2015-10-06 21:26:24 -0400

    Add add to list button to show details; Add JS and checkboxes to episode list

--- app/Http/Controllers/ShowsDetailsController.php
+++ app/Http/Controllers/ShowsDetailsController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests;
+use App\ListEpisodesWatched;
 use App\Lists;
 use App\Show;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class ShowsDetailsController extends Controller
 {
     public function index($seriesId)
     {
+        $listStatuses = DB::table('list_statuses')->get();
         $show = Show::findOrFail($seriesId);
         $show->Genre = implode(", ", array_filter(explode("|", $show->Genre)));
         $show->SiteRating = Lists::where('series_id', $seriesId)->whereNotNull('rating')->avg('rating');
@@ -28,7 +31,7 @@ class ShowsDetailsController extends Controller
 
         $episodes = DB::table('tvepisodes')
             ->join('tvseasons', 'tvepisodes.seasonid', '=', 'tvseasons.id')
-            ->select('tvepisodes.seriesid', 'season', 'episodenumber', 'episodename')
+            ->select('tvepisodes.id', 'tvepisodes.seriesid', 'season', 'episodenumber', 'episodename')
             ->where('tvepisodes.seriesid', $seriesId)
             ->where('season', '<>', 0)
             ->whereNotNull('episodename')
@@ -36,7 +39,29 @@ class ShowsDetailsController extends Controller
             ->orderBy('episodenumber')
             ->get();
 
-        return view('shows/details', compact('show', 'seasons', 'episodes'));
+        if (Auth::check()) {
+            $user = Auth::user();
+            $showAddButton = !Lists::where('user_id', $user->id)->where('series_id', $show->id)->exists();
+            $epsWatched = ListEpisodesWatched::where('user_id', $user->id)
+                ->where('list.series_id', $seriesId)
+                ->select('list_episodes_watched.episode_id')
+                ->join('tvepisodes', 'list_episodes_watched.episode_id', '=', 'tvepisodes.id')
+                ->join('tvseasons', 'tvepisodes.seasonid', '=', 'tvseasons.id')
+                ->join('tvseries', 'tvepisodes.seriesid', '=', 'tvseries.id')
+                ->join('list', 'list_episodes_watched.list_id', '=', 'list.id')
+                ->join('users', 'list.user_id', '=', 'users.id')
+                ->get();
+
+            foreach ($episodes as $episode) {
+                if ($epsWatched->contains('episode_id', $episode->id)) {
+                    $episode->checked = 'checked';
+                }
+            }
+        } else {
+            $showAddButton = false;
+        }
+
+        return view('shows/details', compact('show', 'seasons', 'episodes', 'listStatuses', 'user', 'showAddButton', 'epsWatched'));
     }
 
     // TODO: Maybe don't need this?
--- resources/views/shows/details.blade.php
+++ resources/views/shows/details.blade.php
@@ -6,7 +6,21 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <h1>{{ $show->SeriesName }}</h1>
+                <h1>
+                    {{ $show->SeriesName }}
+                    @if ($showAddButton)
+                        <small>
+                            <a href="#" class="edit"
+                               data-toggle="modal"
+                               data-target="#addModal"
+                               data-series-id="{{ $show->id }}"
+                               data-series-title="{{ $show->SeriesName }}"
+                               title="Add to List">
+                                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                            </a>
+                        </small>
+                    @endif
+                </h1>
 
                 <ul>
                     <li>Genre: {{ $show->Genre }}</li>
@@ -25,19 +39,96 @@
 
             <div class="col-md-6">
                 <h3>Episodes</h3>
+                <a href="#" id="toggleEpisodes">Show All Episodes</a>
                 @foreach($seasons as $season)
-                    <h4>Season {{ $season->season }}</h4>
-                    <ol>
-                        @foreach($episodes as $episode)
-                            @if($episode->season === $season->season)
-                                <li>
-                                    {!! link_to_route('shows/episode', $episode->episodename, ['seriesId' => $episode->seriesid, 'seasonNum' => $season->season, 'episodeNum' => $episode->episodenumber]) !!}
-                                </li>
-                            @endif
-                        @endforeach
-                    </ol>
+                    <h4 class="seasons"><a href="#" class="seasons">Season {{ $season->season }}</a></h4>
+                    <div class="episodes">
+                        <ol>
+                            @foreach($episodes as $episode)
+                                @if($episode->season === $season->season)
+                                    <li>
+                                        <input type="checkbox" id="{{ $episode->id }}" class="episode" {{ $episode->checked or '' }} />
+                                        {!! link_to_route('shows/episode', $episode->episodename, ['seriesId' => $episode->seriesid, 'seasonNum' => $season->season, 'episodeNum' => $episode->episodenumber]) !!}
+                                    </li>
+                                @endif
+                            @endforeach
+                        </ol>
+                    </div>
                 @endforeach
             </div>
         </div>
+
+        @if (Auth::check())
+            <!-- AddModal -->
+            <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                                        aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title" id="addModalLabel"></h4>
+                        </div>
+                        <div class="modal-body">
+                            {!! Form::open(['route' => ['profile/addToList', $user->username], 'class' => 'form-horizontal']) !!}
+
+                            {!! Form::hidden('series_id', null, ['id' => 'series_id']) !!}
+                            {!! Form::hidden('series_name', null, ['id' => 'series_name']) !!}
+
+                            <div class="form-group">
+                                {!! Form::label('status', 'Status: ', ['class' => 'col-sm-2 control-label']) !!}
+
+                                <div class="col-sm-10">
+                                    @foreach($listStatuses as $listStatus)
+                                        <div class="radio">
+                                            <label>
+                                                {!! Form::radio('status', $listStatus->list_status, null, ['id' => 'status_' . $listStatus->list_status]) !!}
+                                                {{ $listStatus->description }}
+                                            </label>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            {!! Form::submit('Add Show To List', ['class' => 'btn btn-primary']) !!}
+                        </div>
+
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        @endif
     </div>
+@stop
+
+@section('javascript')
+    <script>
+        $(document).ready(function() {
+            $('.seasons').next().hide();
+            $('.seasons').click(function () {
+                $(this).next().toggle();
+            });
+
+            $('#toggleEpisodes').click(function () {
+                if ($(this).text() === 'Show All Episodes') {
+                    $(this).text('Hide All Episodes');
+                    $('.episodes').show();
+                } else {
+                    $(this).text('Show All Episodes');
+                    $('.episodes').hide();
+                }
+            });
+
+            $('#addModal').on('show.bs.modal', function (event) {
+                var button = $(event.relatedTarget),
+                        title = button.data('series-title'),
+                        id = button.data('series-id'),
+                        modal = $(this);
+                modal.find('#series_id').val(id);
+                modal.find('#series_name').val(title);
+                modal.find('.modal-title').text(title);
+                modal.find('#status_0').prop('checked', true);
+            });
+    </script>
 @stop
