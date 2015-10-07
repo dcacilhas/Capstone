c247af8e46198e226da5375d5fe275528853035f
David Cacilhas <dcacilhas@gmail.com>
2015-10-06 21:23:25 -0400

    Add ListEpisodesWatched model

---
+++ app/ListEpisodesWatched.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ListEpisodesWatched extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'list_episodes_watched';
+}
