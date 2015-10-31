cc1a0b1205e8199e95bed3432b6c81dfc67d5d2e
David Cacilhas <dcacilhas@gmail.com>
2015-10-30 21:15:13 -0400

    Add 1000 users to seeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -38,6 +38,26 @@ class UserTableSeeder extends Seeder
             ]);
         }
 
+        for ($i = 0; $i < 1000; $i++) {
+            $username = $faker->firstName . ' ' . $faker->lastName;
+            $data[] = [
+                'username' => $username,
+                'email' => strtolower(str_replace(' ', '', $username)) . '@gmail.com',
+                'password' => bcrypt('password'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'about' => $faker->optional()->paragraph($nbSentences = rand(1, 5)),
+                'birthday' => $faker->optional()->dateTimeThisCentury($max = 'now'),
+                'location' => $faker->optional()->city,
+                'gender' => $faker->optional()->randomElement($array = array('M', 'F')),
+                'notification_email' => rand(0, 1),
+                'profile_visibility' => rand(0, 2),
+                'list_visibility' => rand(0, 2)
+            ];
+        }
+
+        DB::table('users')->insert($data);
+
         User::rebuildMapping();
         User::reindex();
     }
